ed3d3c04d609079c648888341a9e25c9514b45c2
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-07-23 16:03:26 +0400

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -ex
 
+update-grub
 
+#ls -Rlo /boot/
 
-sed -i 's,UUID=[^[:blank:]]*,/dev/sda1,' /etc/fstab
-sed -i 's,UUID=[^[:blank:]]*,/dev/sda1,' /boot/grub/grub.conf
+#sed -i 's,UUID=[^[:blank:]]*,/dev/sda1,' /etc/fstab
+#sed -i 's,UUID=[^[:blank:]]*,/dev/sda1,' /boot/grub/menu.lst
 
