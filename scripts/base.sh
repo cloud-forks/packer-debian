a2ff7266af32c1b36b73e6d6e44e91f89f6cb4bc
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-08-21 18:29:47 +0400

    disable cloud-init
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -22,3 +22,33 @@ iface eth0 inet6 auto
 iface eth0 inet dhcp
 dns-nameservers 2001:4860:4860::8888 2001:4860:4860::8844 8.8.8.8 8.8.4.4
 EOF
+
+export APT_LISTCHANGES_FRONTEND=none
+export DEBIAN_FRONTEND=noninteractive
+
+for p in vim-tiny vim-common tcpd debian-faq discover discover-data doc-debian laptop-detect ftp telnet wamerican w3m sudo reportbug procmail mutt mlocate ispell dictionaries-common ; do
+    apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy purge $p || :
+done
+
+sed -i 's|#GRUB_DISABLE_LINUX_UUID.*|GRUB_DISABLE_LINUX_UUID=true|g' /etc/default/grub
+sed -i 's|#GRUB_DISABLE_RECOVERY.*|GRUB_DISABLE_RECOVERY=true|g' /etc/default/grub
+update-initramfs -k all -u
+update-grub
+
+: <<COMMENT
+apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy install cloud-init cloud-initramfs-growroot
+
+cat <<EOF > /etc/cloud/cloud.cfg.d/50_allow_root.cfg
+users: []
+disable_root: 0
+ssh_pwauth: 1
+EOF
+
+cat <<EOF >> /etc/cloud/cloud.cfg
+# Documentation on data sources configuration options
+datasource:
+  Ec2:
+    timeout : 5
+    max_wait : 10
+EOF
+COMMENT
