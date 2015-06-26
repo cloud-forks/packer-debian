7beaaf7dafd0d20ceadd880577fbf433d2ad6a86
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-06-26 17:40:31 +0300

    fix reboot
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,4 +1,3 @@
 #!/bin/sh -x
 
-grep -q "Debian GNU/Linux 8" /etc/os-release && nohup /bin/sh -c 'reboot;' &
-grep -q "Debian GNU/Linux 7" /etc/os-release && reboot
+nohup "/bin/sh -c 'sleep 2s; reboot;'" &
